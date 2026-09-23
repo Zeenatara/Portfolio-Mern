b84c3fc8c516b5a36414999d3a06b627fef08eb8
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-09-23 20:46:15 +0530

    feat: build portfolio navigation

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <div className="brand">ZA</div>
+        <div className="nav-center">arazeenat747@gmail.com</div>
+        <div className="nav-links">
+          <a href="#about">ABOUT</a>
+          <a href="#work">WORK</a>
+          <a href="#contact">CONTACT</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
